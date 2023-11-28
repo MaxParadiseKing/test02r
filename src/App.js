fc3a02fc50d5d7763ec976bc7fdb953c08016407
Igor <118084755+MaxParadiseKing@users.noreply.github.com>
2023-11-29 01:08:07 +0300

    continue progect 54:20

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import Counter from "./component/Counter";
+import ClassCounter from "./component/ClassCounter";
+import './Styles/app.css';
+import PostItem from "./component/PostItem";
+import PostList from "./component/PostList";
+import MyButton from "./component/UI/button/MyButton";
+import MyInput from "./component/UI/input/MyInput";
+// import PostForm from "./component/PostForm";
+
+
 
 function App() {
+ const [posts, setPosts] = useState ([
+  {id: 1, title: 'Javascript', body: 'Description'},
+  {id: 2, title: 'Javascript 2', body: 'Description'},
+  {id: 3, title: 'Javascript 3', body: 'Description'},
+ ])
+
+ const [title, setTitle] = useState ( '')
+ const [body, setBody] = useState ( '')
+
+ const addNewPost=(e)=> {
+  e.preventDefault()
+        const newPost = {
+          title,
+          body
+        }
+        console.log(newPost)
+ }
+
   return (
     <div className="App">
-    </div>
+      <form>
+        {/* Управляемый компанент */}
+        <MyInput 
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        type ="Text" 
+        placeholder="Название поста"/>
+        {/* Не упроавляемый компанент */}
+        <MyInput 
+        value={body}
+        onChange={e => setBody(e.target.value)}
+        type ="Text" 
+        placeholder="Описание поста"/>
+        <MyButton onClick={addNewPost}> Создать пост </MyButton>
+      </form>
+         <PostList posts={posts} title='Посты про JS'/>
+    </div> 
   );
 }
 
---
+++ src/component/PostForm.jsx
@@ -0,0 +1,39 @@
+// import React from "react";
+// import MyInput from "./UI/input/MyInput";
+// import MyButton from "./UI/button/MyButton";
+
+// const PostForm = ({create}) => {
+//     const [post, setPost] = useState ({title: '', body: ''})
+
+
+//     const addNewPost=(e)=> {
+//         e.preventDefault()
+//               const newPost = {
+//                 ...post, id: Date.now()
+//               }
+//               create(newPost)
+//               setPost({title: '', body: ''})
+//        }
+       
+//     return (
+        
+//             <form>
+//         {/* Управляемый компанент */}
+//         <MyInput 
+//         value={post.title}
+//         onChange={e => setPost({...post, title: e.target.value})}
+//         type ="Text" 
+//         placeholder="Название поста"/>
+//         {/* Не упроавляемый компанент */}
+//         <MyInput 
+//         value={post.body}
+//         onChange={e => setPost({...post, body: e.target.value})}
+//         type ="Text" 
+//         placeholder="Описание поста"/>
+//         <MyButton onClick={addNewPost}> Создать пост </MyButton>
+//       </form>
+        
+//     );
+// }
+
+// export default PostForm;
